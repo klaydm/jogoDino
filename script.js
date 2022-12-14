70153413f87d453807ed393f3970cd058c9a0ade
Klaydm <klaydm@gmail.com>
2022-12-13 22:44:15 -0300

    Iniciando o jogo Dino, criadas variáveis

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () =>{
+  const dino = document.querySelector("dino")
+  const grid = document.querySelector("grid")
+  const body = document.querySelector("body")
+  const alert = document.querySelector("alert")
+
+  let jumping = false
+  let gravity = 0.9
+  let gameo = false
+  let dinopy = 0
+})
